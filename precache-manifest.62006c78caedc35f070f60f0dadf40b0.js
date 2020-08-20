8046fae45160c78c4351ee680407376733dadf9f
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-19 23:03:17 -0700

    Updates

---
+++ precache-manifest.62006c78caedc35f070f60f0dadf40b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33de5eb308ed6bfdc4f8f4f97b9be89",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "9c417774a2038169bcb9",
+    "url": "/reactPortfolio/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "bf7a8b253b10adb1b701",
+    "url": "/reactPortfolio/static/css/main.fee34c74.chunk.css"
+  },
+  {
+    "revision": "9c417774a2038169bcb9",
+    "url": "/reactPortfolio/static/js/2.ebf487c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactPortfolio/static/js/2.ebf487c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7a8b253b10adb1b701",
+    "url": "/reactPortfolio/static/js/main.29e49e8f.chunk.js"
+  },
+  {
+    "revision": "24736e6795fa0c1201b3",
+    "url": "/reactPortfolio/static/js/runtime-main.21fff2c0.js"
+  },
+  {
+    "revision": "0c5bcfe6aa77eb0c4835d669ebdd207b",
+    "url": "/reactPortfolio/static/media/Isaias Resume 792020.0c5bcfe6.pdf"
+  },
+  {
+    "revision": "2b583042bfd4f01833684908f93354c9",
+    "url": "/reactPortfolio/static/media/me.2b583042.png"
+  }
+]);
