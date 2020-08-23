628edec10283203f16d15a263fbc1fcf34475c0f
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-23 13:29:14 -0700

    fixed navbar

--- portfolio/src/component/navbar/index.js
+++ portfolio/src/component/navbar/index.js
@@ -5,7 +5,7 @@ function navbar() {
     return (
         <nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center bg-secondary" id="nav">
         <div className="container">
-            {/* <a className="navbar-brand js-scroll-trigger"></a> */}
+            <a className="navbar-brand js-scroll-trigger"></a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i className="fas fa-bars"></i>
